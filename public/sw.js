47f6b23c48aa4aec3d6468dca91dc4a4f98dbedc
セイントチヌ <riskyedge7366@gmail.com>
2026-08-19 20:54:56 +0900

    Add dark mahjong portraits

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chinuquest2-v117';
+const CACHE_NAME = 'chinuquest2-v118';
 const APP_SHELL = ['./', './manifest.webmanifest', './icons/danballman-icon-192.png', './icons/danballman-icon-512.png'];
 
 self.addEventListener('install', (event) => {
